9b5d868a60aa1dee2e1985fb7c24e6968859d10c
Tejas150 <tejaskamble1629@gmail.com>
2024-08-03 19:54:22 +0530

    Add avatars to display markers

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,90 +1,106 @@
-const socket = io()
+// script.js
 
-if(navigator.geolocation) {
-    navigator.geolocation.watchPosition(
-        // callback
-        (position) => {
-        const { latitude, longitude } = position.coords
-        socket.emit('send-location', { latitude, longitude })
+// Set up socket.io connection
+const socket = io();
 
-        },
-        // error
-        (error) => {
-            console.error(error)
-        },
-        // options
-        {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        }
-    )
+// Set up map and markers
+const map = L.map('map').setView([0, 0], 16);
+const markers = {};
+let currentMarker = null;
+
+// Set up tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map',
+}).addTo(map);
+
+const iconUrl = `assets/${getRandomNumber()}.png`
+// Set up custom icon
+const customIcon = L.icon({
+  iconUrl: iconUrl,
+  iconSize: [50, 50],
+  iconAnchor: [25, 25],
+  popupAnchor: [0, -30],
+  tooltipAnchor: [20, -20],
+  className: 'custom-icon',
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 9) + 1;
 }
 
-const map = L.map('map').setView([0,0], 16)
+// Function to create a new marker
+function createMarker(id, latitude, longitude) {
+  const marker = L.marker([latitude, longitude], { icon: customIcon });
+  marker.addTo(map);
+  markers[id] = marker;
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: 'Map'
-    }
-).addTo(map)
+  // Add event listener to marker
+  marker.on('click', () => {
+    // Scale up the marker
+    marker.setIcon(L.icon({
+      iconUrl: iconUrl,
+      iconSize: [100, 100],
+      iconAnchor: [50, 50],
+      popupAnchor: [0, -60],
+      tooltipAnchor: [40, -40],
+      className: 'custom-icon',
+    }));
+    currentMarker = marker;
+  });
+}
 
-const routingControlLayer = L.layerGroup().addTo(map);
+// Function to update a marker's position
+function updateMarker(id, latitude, longitude) {
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  }
+}
 
-const markers = {}
-let routingControls = {}
+// Function to remove a marker
+function removeMarker(id) {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+}
 
-socket.on('recieve-location', ({id, latitude, longitude}) => {
-    map.setView([latitude, longitude])
+// Function to reset the current marker
+function resetCurrentMarker() {
+  if (currentMarker) {
+    currentMarker.setIcon(customIcon);
+    // currentMarker = null;
+  }
+}
 
-    if(markers[id]) {
-        markers[id].setLatLng([latitude, longitude])
-    }
-    else {
-        markers[id] = L.marker([latitude, longitude]).addTo(map)
-    }
-
-    // get the current user's marker
-    const currentUserMarker = markers[socket.id]
-
-    // if the current user's marker exists, draw a route to the new marker
-    if (currentUserMarker) {
-        Object.keys(markers).forEach(id => {
-            if(id != socket.id) {
-                if (routingControls[id]) {
-                    // Update the existing route
-                    routingControls[id].setWaypoints([
-                        L.latLng(currentUserMarker.getLatLng().lat, currentUserMarker.getLatLng().lng),
-                        L.latLng(markers[id].getLatLng().lat, markers[id].getLatLng().lng)
-                    ])
-                } else {
-                    // Create a new route
-                    routingControls[id] = L.Routing.control({
-                        waypoints: [
-                            L.latLng(currentUserMarker.getLatLng().lat, currentUserMarker.getLatLng().lng),
-                            L.latLng(markers[id].getLatLng().lat, markers[id].getLatLng().lng)
-                        ],
-                        router: L.Routing.osrmv1({
-                            serviceUrl: 'https://router.project-osrm.org/route/v1'
-                        }),
-                        routeWhileDragging: false
-                    });
-                    // Add the control to the map instead of the layer group
-                    map.addControl(routingControls[id]);
-                }
-            }
-        })
-    }
-})
+// Set up event listeners for socket.io events
+socket.on('recieve-location', ({ id, latitude, longitude }) => {
+  map.setView([latitude, longitude]);
+  createMarker(id, latitude, longitude);
+});
 
 socket.on('user-disconnected', (id) => {
-    if(markers[id]) {
-        map.removeLayer(markers[id])
-        delete markers[id]
-    }
+  removeMarker(id);
+});
 
-    if (routingControls[id]) {
-        // Remove the control from the map
-        map.removeControl(routingControls[id])
-        delete routingControls[id] 
-    }
-})
+// Set up event listener for map click
+map.on('click', () => {
+  resetCurrentMarker();
+});
+
+// Set up geolocation
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit('send-location', { latitude, longitude });
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    },
+  );
+}
